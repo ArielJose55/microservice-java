ed3cb1710a79ce3bcef2ebec4990225bbf258ae1
Ariel Arnedo <arielarnedo@seven4n.com>
2019-04-12 23:20:54 -0500

    script base de el micro personas

---
+++ hardware-layer/database/person/init-database.sql
@@ -0,0 +1,91 @@
+CREATE SEQUENCE public."NATURAL_PERSON_id_seq";
+
+ALTER SEQUENCE public."NATURAL_PERSON_id_seq"
+    OWNER TO person_user;
+
+CREATE SEQUENCE public."RESIDENT_id_seq";
+
+ALTER SEQUENCE public."RESIDENT_id_seq"
+    OWNER TO person_user;
+
+CREATE SEQUENCE public."PET_id_seq";
+
+ALTER SEQUENCE public."PET_id_seq"
+    OWNER TO person_user;
+
+CREATE TABLE public."PERSON"
+(
+    identification character varying(25) COLLATE pg_catalog."default" NOT NULL,
+    "typeIdentification" character varying(25) COLLATE pg_catalog."default",
+    CONSTRAINT "PERSON_pkey" PRIMARY KEY (identification)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."PERSON"
+    OWNER to person_user;
+
+
+CREATE TABLE public."NATURAL_PERSON"
+(
+    id bigint NOT NULL DEFAULT nextval('"NATURAL_PERSON_id_seq"'::regclass),
+    name character varying(120) COLLATE pg_catalog."default",
+    last_name character varying(120) COLLATE pg_catalog."default",
+    person_fk character varying(25) COLLATE pg_catalog."default",
+    CONSTRAINT "NATURAL_PERSON_pkey" PRIMARY KEY (id),
+    CONSTRAINT "NATURAL_PERSON_person_fk_key" UNIQUE (person_fk)
+,
+    CONSTRAINT "NATURAL_PERSON_person_fk_fkey" FOREIGN KEY (person_fk)
+        REFERENCES public."PERSON" (identification) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."NATURAL_PERSON"
+    OWNER to person_user;
+
+
+CREATE TABLE public."RESIDENT"
+(
+    id bigint NOT NULL DEFAULT nextval('"RESIDENT_id_seq"'::regclass),
+    type character varying(25) COLLATE pg_catalog."default",
+    person_natural_fk character varying(25) COLLATE pg_catalog."default",
+    CONSTRAINT "RESIDENT_pkey" PRIMARY KEY (id),
+    CONSTRAINT "RESIDENT_person_natural_fk_fkey" FOREIGN KEY (person_natural_fk)
+        REFERENCES public."NATURAL_PERSON" (person_fk) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."RESIDENT"
+    OWNER to person_user;
+
+CREATE TABLE public."PET"
+(
+    id bigint NOT NULL DEFAULT nextval('"PET_id_seq"'::regclass),
+    name character varying(75) COLLATE pg_catalog."default",
+    species character varying(75) COLLATE pg_catalog."default",
+    resident_fk character varying(21) COLLATE pg_catalog."default",
+    CONSTRAINT "PET_pkey" PRIMARY KEY (id),
+    CONSTRAINT "PET_resident_fk_fkey" FOREIGN KEY (resident_fk)
+        REFERENCES public."RESIDENT" (person_natural_fk) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."PET"
+    OWNER to person_user;
